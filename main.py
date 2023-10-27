70cb35c38a4ab876efd9027f2048e1923977390c
aidenhock <aiden.hock@gmail.com>
2023-10-26 17:51:02 -0700

    initializing
    adding index and main

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import qrcode
+from io import BytesIO
+from base64 import b64encode
+
+
+
+
+app = Flask(__name__)
+
+
+#url hompage/ what page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#method generateQR sends POST request
+@app.route('/', methods=['POST'])
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get('link')
+    #pass link to qrcode maker library
+    img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+
+    base64_img = "data:image/png;base64," + \
+        b64encode(memory.getvalue()).decode('ascii')
+    
+    return render_template('index.html', data=base64_img)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
